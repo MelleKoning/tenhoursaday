789df20b0c41ca95161294b75bb1c6f9531a1778
Hace <mellekoning@gmail.com>
2020-05-31 21:19:06 +0200

    fix calculation

--- cmd/tenhoursaday.go
+++ cmd/tenhoursaday.go
@@ -15,8 +15,8 @@ into a 10 hour, 100 minute, 100 seconds clock
 func main() {
 	fmt.Println("Hallo")
 
-	hour, minute, seconds := time.Now().Clock()
-	fmt.Printf("Uren: %v, Minuten: %v, Seconden: %v\n", hour, minute, seconds)
+	currentTime := time.Now()
+	fmt.Printf("Uren: %v, Minuten: %v, Seconden: %v\n", currentTime.Hour(), currentTime.Minute(), currentTime.Second())
 
 	//secFromMidnight := hour*3600 + minute*60 + seconds
 
@@ -29,29 +29,32 @@ func main() {
 
 	stop := false
 	times := 0
+	fmt.Print("\033[s") // save the cursor position
+	startTime := time.Now()
 	for !stop {
-		hour, minute, seconds := time.Now().Clock()
-		secFromMidnight := hour*3600 + minute*60 + seconds
-		secInHundreds := float64(secFromMidnight) * fraction
-		Print100Time(secInHundreds)
-		time.Sleep(100 * time.Millisecond)
+		currentTime := time.Now()
+		millisecFromMidnight := currentTime.Hour()*3600000 + currentTime.Minute()*60000 + currentTime.Second()*1000 + currentTime.Nanosecond()/1000000
+		millisecInHundreds := float64(millisecFromMidnight) * fraction
+		Print100Time(currentTime, millisecInHundreds)
+		time.Sleep(50 * time.Millisecond)
 		times += 1
-		if times > 100 {
+		if time.Now().Sub(startTime) > time.Minute*1 {
 			stop = true
 		}
 	}
 
 }
 
-func Print100Time(secInHundreds float64) {
-	hour100Float := secInHundreds / 10000
-	minutes100Float := (secInHundreds - 10000*math.Floor(hour100Float)) / 100
-	seconds100Float := (secInHundreds - (10000*math.Floor(hour100Float) + 100*math.Floor(minutes100Float)))
+func Print100Time(currentTime time.Time, milli100 float64) {
+
+	hour100Float := milli100 / 10000000
+	minutes100Float := (milli100 - 10000000*math.Floor(hour100Float)) / 100000
+	seconds100Float := (milli100 - (10000000*math.Floor(hour100Float) + 100000*math.Floor(minutes100Float))) / 1000
 
 	uur100 := int32(math.Floor(hour100Float))
 	minute100 := int32(math.Floor(minutes100Float))
 	sec100 := (int32)(math.Floor(seconds100Float))
-
-	fmt.Printf("%02d:%03d:%03d\n", uur100, minute100, sec100)
+	fmt.Print("\033[u\033[K")
+	fmt.Printf("%02d:%02d:%02d - %02d:%02d:%02d\n", currentTime.Hour(), currentTime.Minute(), currentTime.Second(), uur100, minute100, sec100)
 
 }
